44cf5fb2656bddc093250311f6c85eab1166a2dc
asadov <jiexaf@gmail.com>
2019-11-27 15:56:09 +0300

    Коммит по скрипту каталона 4(параметры в методы).

--- test_add_group.py
+++ test_add_group.py
@@ -13,27 +13,27 @@ class TestAddGroup(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/group.php")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("content").click()
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def init_group_creation(self, wd):
         wd.find_element_by_name("new").click()
 
-    def fill_group_form(self, wd):
+    def fill_group_form(self, wd, name, header, footer):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test1")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("test2")
+        wd.find_element_by_name("group_footer").send_keys(footer)
 
     def submit_group_creation(self, wd):
         wd.find_element_by_name("submit").click()
@@ -47,22 +47,22 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.init_group_creation(wd)
-        self.fill_group_form(wd)
+        self.fill_group_form(wd, name="test", header="test1", footer="test2")
         self.submit_group_creation(wd)
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.init_group_creation(wd)
+        self.fill_group_form(wd, name="", header="", footer="")
+        self.submit_group_creation(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
