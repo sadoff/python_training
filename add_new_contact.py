948a630ff33bc609351307385fdea390b4207ee8
asadov <jiexaf@gmail.com>
2019-11-28 16:08:30 +0300

    Коммит по скрипту добавления нового контакта 4(продолжаю выделять вспомогательные методы).

--- add_new_contact.py
+++ add_new_contact.py
@@ -12,47 +12,48 @@ class AddNewContact(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, userlogin, userpassword):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(userlogin)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(userpassword)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def create_contact(self, wd):
+    def create_contact(self, wd, firstname, middlename, lastname, nickname, adress, mobilephone, email, bday, bmonth,
+                       byear):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Test")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Testovich")
+        wd.find_element_by_name("middlename").send_keys(middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Testov")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("test1")
+        wd.find_element_by_name("nickname").send_keys(nickname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Testovia, Test City, Test street")
+        wd.find_element_by_name("address").send_keys(adress)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("+79999999999")
+        wd.find_element_by_name("mobile").send_keys(mobilephone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("test@test.ts")
+        wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("9")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(bday)
         wd.find_element_by_name("bday").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("August")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(bmonth)
         wd.find_element_by_name("bmonth").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1990")
+        wd.find_element_by_name("byear").send_keys(byear)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def return_to_home_page(self, wd):
@@ -64,8 +65,9 @@ class AddNewContact(unittest.TestCase):
     def test_add_new_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.create_contact(wd)
+        self.login(wd, userlogin="admin", userpassword="secret")
+        self.create_contact(wd, firstname="Test", middlename="Testovich", lastname="Testov", nickname="test1", adress="Testovia, Test City, Test street",
+                            mobilephone="+79999999999", email="test@test.ts", bday="9", bmonth="August", byear="1990")
         self.return_to_home_page(wd)
         self.logout(wd)
 
