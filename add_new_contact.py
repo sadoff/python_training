c24ef5cf918cc005e4ce35cddb45259ff8d342ab
asadov <jiexaf@gmail.com>
2019-11-28 15:31:17 +0300

    Коммит по скрипту добавления нового контакта 2(чистка скрипта от "лишних" строк).

--- add_new_contact.py
+++ add_new_contact.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
-    def test_add_new_contact(self):
+     def test_add_new_contact(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
@@ -57,27 +52,6 @@ class AddNewContact(unittest.TestCase):
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         wd.find_element_by_link_text("home page").click()
         wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
     
     def tearDown(self):
         self.wd.quit()
