826888d75a70ad5c3de41584a033082caae76b6f
asadov <jiexaf@gmail.com>
2019-12-03 17:01:52 +0300

    Коммит №11 выделяем класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -7,32 +8,11 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
-
-    def init_group_creation(self):
-        wd = self.wd
-        wd.find_element_by_name("new").click()
-
-    def fill_group_form(self, group):
-        wd = self.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-
-    def submit_group_creation(self):
-        wd = self.wd
-        wd.find_element_by_name("submit").click()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
 
     def terminate(self):
         self.wd.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -15,10 +15,10 @@ def app(request):
 
 def test_add_group(app):
         app.session.login(username="admin", password="secret")
-        app.init_group_creation()
-        app.fill_group_form(Group(name="test", header="test1", footer="test2"))
-        app.submit_group_creation()
-        app.return_to_groups_page()
+        app.group.init_creation()
+        app.group.fill_form(Group(name="test", header="test1", footer="test2"))
+        app.group.submit_creation()
+        app.group.return_to_groups_page()
         app.session.logout()
 
 
