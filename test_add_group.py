11050a70d0bbd307329e4493f41d263da1cb8e9c
asadov <jiexaf@gmail.com>
2019-12-03 13:47:40 +0300

    Коммит по скрипту добавления нового контакта 6(выделил в отдельный класс параметры контакта и вынес это в отдельный файл).

--- test_add_group.py
+++ test_add_group.py
@@ -9,10 +9,13 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -22,10 +25,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_id("content").click()
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def init_group_creation(self, wd):
+    def init_group_creation(self):
+        wd = self.wd
+        self.init_group_creation()
         wd.find_element_by_name("new").click()
 
-    def fill_group_form(self, wd, group):
+    def fill_group_form(self, group):
+        wd = self.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -34,34 +40,32 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
 
-    def submit_group_creation(self, wd):
+    def submit_group_creation(self):
+        wd = self.wd
+        self.submit_group_creation()
         wd.find_element_by_name("submit").click()
 
-    def return_to_groups_page(self, wd):
+    def return_to_groups_page(self):
+        wd = self.wd
+        self.return_to_groups_page()
         wd.find_element_by_link_text("group page").click()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.init_group_creation(wd)
-        self.fill_group_form(wd, Group(name="test", header="test1", footer="test2"))
-        self.submit_group_creation(wd)
-        self.return_to_groups_page(wd)
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.fill_group_form(Group(name="test", header="test1", footer="test2"))
+        self.return_to_groups_page()
+        self.logout()
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.init_group_creation(wd)
-        self.fill_group_form(wd, Group(name="", header="", footer=""))
-        self.submit_group_creation(wd)
-        self.return_to_groups_page(wd)
-        self.logout(wd)
+        self.login( username="admin", password="secret")
+        self.fill_group_form(Group(name="", header="", footer=""))
+        self.logout()
 
     def tearDown(self):
         self.wd.quit()
