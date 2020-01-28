6062a1bbfa58a7b9822936584454a4ef5b90c9fb
asadov <jiexaf@gmail.com>
2020-01-28 17:54:10 +0300

    Коммит №30 Реализовал загрузку информации о тестируемом приложении из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -1,22 +1,24 @@
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
 
 fixture = None
-
+target = None
 
 @pytest.fixture(scope="session")
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -30,6 +32,6 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook")
+    parser.addoption("--target", action="store", default="target.json")
 
 
