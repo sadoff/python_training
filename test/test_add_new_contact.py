cf844ddb4a069848cb8be71f2f838c52c8578638
asadov <jiexaf@gmail.com>
2020-01-30 13:32:59 +0300

    Коммит №32 Реализовал загрузку тестовых данных из файлов

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -4,8 +4,9 @@ from generator.contact import testdata
 import pytest
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_new_contact(app, contact):
+#@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_new_contact(app, json_contact):
+    contact = json_contact
     old_contacts = app.contact.get_contact_list()
     app.contact.add_contact(contact)
     assert len(old_contacts) +1 == app.contact.count()
